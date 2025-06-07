283ee9cb4dccde591263525c92fc7c84e46270b6
Náthally <nathallyfernandes@ufmg.br>
2025-06-07 17:56:26 -0300

    Exercicio 4

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,102 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+  it('Edita uma tarefa existente', () => {
+  cy.visit('');
+  
+  // Adiciona uma tarefa
+  cy.get('[data-cy=todo-input]')
+    .type('Tarefa para editar{enter}');
+  
+  // Verifica se a tarefa foi adicionada
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 1)
+    .first()
+    .should('have.text', 'Tarefa para editar');
+  
+  // Edita a tarefa com duplo clique
+  cy.get('[data-cy=todos-list] > li label')
+    .first()
+    .dblclick();
+  
+  // Apaga o texto atual e digita o novo texto
+  cy.get('[data-cy=todos-list] > li .edit')
+    .clear()
+    .type('Tarefa editada{enter}');
+  
+  // Verifica se a tarefa foi editada
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .first()
+    .should('have.text', 'Tarefa editada');
+});
+  it('Verifica o contador de itens restantes', () => {
+  cy.visit('');
+  
+  // Adiciona três tarefas
+  cy.get('[data-cy=todo-input]')
+    .type('Tarefa 1{enter}')
+    .type('Tarefa 2{enter}')
+    .type('Tarefa 3{enter}');
+  
+  // Verifica se o contador mostra 3 itens
+  cy.get('.todo-count')
+    .should('contain', '3 items left');
+  
+  // Marca uma tarefa como concluída
+  cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+    .first()
+    .click();
+  
+  // Verifica se o contador agora mostra 2 itens
+  cy.get('.todo-count')
+    .should('contain', '2 items left');
+  
+  // Marca outra tarefa como concluída
+  cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+    .eq(1)
+    .click();
+  
+  // Verifica se o contador agora mostra 1 item (singular)
+  cy.get('.todo-count')
+    .should('contain', '1 item left');
+});
+  it('Limpa todas as tarefas concluídas', () => {
+  cy.visit('');
+  
+  // Adiciona três tarefas
+  cy.get('[data-cy=todo-input]')
+    .type('Tarefa 1{enter}')
+    .type('Tarefa 2{enter}')
+    .type('Tarefa 3{enter}');
+  
+  // Marca duas tarefas como concluídas
+  cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+    .first()
+    .click();
+  
+  cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+    .eq(1)
+    .click();
+  
+  // Verifica se existem 3 tarefas no total
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 3);
+  
+  // Clica no botão "Clear completed"
+  cy.get('.clear-completed')
+    .click();
+  
+  // Verifica se apenas a tarefa não concluída permanece
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 1);
+  
+  // Verifica se o contador mostra 1 item
+  cy.get('.todo-count')
+    .should('contain', '1 item left');
+});
+
 });
